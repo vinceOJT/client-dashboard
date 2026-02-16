1096288dd7bc4fdb5868de7995f0f386fcd118fa
vinceOJT <jaca.ojt@gmail.com>
2026-02-17 04:27:05 +0800

    displaying dummy data to dashboard

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
         fink:
-          "bg-pink-100 text-pink-400 hover:bg-pink/80",
+          "bg-fink-color text-pink-400 hover:bg-fink-color/80 red-",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
